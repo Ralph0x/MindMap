0b1e9ca8400a92935975d4d3f80899caecf8158a
Ralph0x <163339207+Ralph0x@users.noreply.github.com>
2024-07-06 04:45:17 +0300

    clean code

--- mindmaps.py
+++ mindmaps.py
@@ -7,52 +7,59 @@ load_dotenv()
 FILE_PATH = os.getenv('MIND_MAP_FILE_PATH', 'mind_maps.json')
 
 class MindMap:
+
     @staticmethod
-    def load_mind_maps():
+    def _read_or_create_file():
+        """Internal method to read mind map data or create file if it doesn't exist."""
         try:
             with open(FILE_PATH, 'r') as file:
                 return json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
+        except FileNotFoundError:
+            with open(FILE_PATH, 'w') as file:
+                json.dump({}, file)
+            return {}
+        except json.JSONDecodeError:
             return {}
 
     @staticmethod
-    def save_mind_maps(mind_maps):
+    def _write_data(mind_maps):
+        """Internal method to write data to the file."""
         with open(FILE_PATH, 'w') as file:
             json.dump(mind_maps, file, indent=4)
 
     @classmethod
     def create_mind_map(cls, map_id, mind_map):
-        mind_maps = cls.load_mind_maps()
+        mind_maps = cls._read_or_create_file()
 
         if map_id in mind_maps:
             raise ValueError('MindMap with the given ID already exists.')
 
-        mind_maps[map_id] = mind_map
-        cls.save_mind_maps(mind_maps)
+        mind_maps[map_id] = mind___map
+        cls._write_data(mind_maps)
 
     @classmethod
     def update_mind_map(cls, map_id, mind_map):
-        mind_maps = cls.load_mind_maps()
+        mind_maps = cls._read_or_create_file()
 
         if map_id not in mind_maps:
             raise ValueError('MindMap does not exist and cannot be updated.')
 
         mind_maps[map_id] = mind_map
-        cls.save_mind_maps(mind_maps)
+        cls._write_data(mind_maps)
 
     @classmethod
     def delete_mind_map(cls, map_id):
-        mind_maps = cls.load_mind_maps()
+        mind_maps = cls._read_or_create_file()
 
         if map_id in mind_maps:
             del mind_maps[map_id]
-            cls.save_mind_maps(mind_maps)
+            cls._write_data(mind_maps)
         else:
             raise ValueError('MindMap does not exist and cannot be deleted.')
 
     @classmethod
     def get_mind_map(cls, map_id):
-        mind_maps = cls.load_mind_maps()
+        mind_maps = cls._read_or_create_file()
 
         if map_id in mind_maps:
             return mind_maps[map_id]
@@ -61,4 +68,4 @@ class MindMap:
 
     @classmethod
     def list_mind_maps(cls):
-        return cls.load_mind_maps()
+        return cls._read_or_create_file()
