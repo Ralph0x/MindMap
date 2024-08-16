d1d924da0442f9553cc498484adedb1ff144f715
Ralph0x <163339207+Ralph0x@users.noreply.github.com>
2024-08-16 14:50:48 +0300

    Generate file Header.js

---
+++ frontend/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/features">
+              Features
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
